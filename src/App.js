78f9e3ee4afd2dbc9e3d21c4f1b9c03a2367513e
MrSofty123 <shaheer1642@gmail.com>
2022-10-17 23:44:39 +0500

    socketio test call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-//import {socket} from './websocket/socket'
+import {socket,socketHasConnected} from './websocket/socket'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  async componentDidMount() {
+    console.log('[App] componentDidMount')
+    await socketHasConnected();
+    socket.emit("ping", 'custom-data-sent-from-client', (data) => console.log('received data from socket', data))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      );
+  }
 }
 
 export default App;
--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -6,13 +6,24 @@ const socket = io(process.env.REACT_APP_SOCKET_URL, {
 
 socket.on("connect", () => {
     console.log('[websocket] connected',socket.id)
-    socket.emit("ping",'custom-data-sent-from-client',(data) => console.log('received data from socket', data))
 });
   
 socket.on("disconnect", () => {
     console.log('[websocket] disconnected')
 });
 
+async function socketHasConnected() {
+    return new Promise((resolve,reject) => {
+        if (socket.connected) return resolve(true)
+        else {
+            socket.on("connect", () => {
+                return resolve(true)
+            });
+        }
+    })
+}
+
 export {
-    socket
+    socket,
+    socketHasConnected
 }
