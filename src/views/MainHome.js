8c5c6f88207c9dbf3b7877b99ca1c9d02c1b47ce
MrSofty123 <shaheer1642@gmail.com>
2022-12-07 00:49:19 +0500

    Update MainHome.js

--- src/views/MainHome.js
+++ src/views/MainHome.js
@@ -243,39 +243,31 @@ class MainHome extends React.Component {
 
         <Grid item xs={12}>
           <div className="App">
-            <Container>
-              <Row xs={3}>
+            <Container 
+              style={{
+                border: "3px solid ",
+                width: "400px",
+                maxHeight: "400px",
+                overflow: "scroll",
+              }}>
+              <Row xs={12}>
                 {this.state.eventsArr.map((event) => {
                   return (
-                    <Col sm={6} md={4} className='mt-3'>
-                      <Card >
-                        {/* <CardImg
-                          top
-                          width="100%"
-                          src="/assets/318x180.svg"
-                          alt="Card image cap"
-                        /> */}
-                        <CardBody>
-                          <Typography style={{textAlign:"center"}}>{event.title}</Typography>
-                          <CardText style={{textAlign:"center"}}>{event.body}</CardText>
-                          <Card.Footer style={{cursor: "pointer",
-                                              textAlign:"center"
-                        }}>
-                          <Button 
-                            onClick={() => {
-                              socket.emit(
-                                "events/delete",
-                                { event_id: event.event_id },
-                                (res) => {}
-                              );
-                            }}
-                          >
-                            Delete
-                          </Button>
-                          </Card.Footer>
-                        </CardBody>
-                      </Card>
-                    </Col>
+                    <div>
+                        <Typography style={{ textAlign: "center" }}>
+                          {event.title}
+                        </Typography>
+                        <Typography style={{ textAlign: "center" }}>
+                          {event.body}
+                        </Typography>
+                        <div
+                          style={{
+                            flex: 1,
+                            height: "1px",
+                            backgroundColor: "orange",
+                          }}
+                        />
+                    </div>
                   );
                 })}
               </Row>
